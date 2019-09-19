ccf2ac4d33b04bead050416f6b0e215322a7e9fe
Aidas Baranauskas <aidas.baranauskas@metasite.net>
2019-09-19 17:42:09 +0300

    module 2

---
+++ GreetingLogic/Greeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreetingLogic
+{
+    public class Greeting
+    {
+        public string GetString()
+        {
+            var greeting = "Hello";
+            int hour = DateTime.Now.Hour;
+
+            if (hour<12)
+            {
+                greeting = "Good mornig";
+            }
+            else if (hour>18)
+            {
+                greeting = "Good evening";
+            }
+
+            return greeting;
+        }
+    }
+}
